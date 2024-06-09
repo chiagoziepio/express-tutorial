89e952294266a840486e09a51a2a3c3e4eb990f3
chiagozie pio <nduagubapio@gmail.com>
2024-06-09 13:23:18 -0700

    added events logger

---
+++ middlewares/logEvent.js
@@ -0,0 +1,27 @@
+const path = require("path")
+const fspromises = require("fs").promises;
+const fs = require("fs");
+const { v4 : uuid } = require("uuid")
+const { format } = require("date-fns")
+
+const logAction = async(msg, logFile)=>{
+ const dateTime = format(new Date(), 'yyyyMMdd\tHH:mm:ss');
+ const logItem = `${dateTime}\t${uuid()}${msg}\n`;
+
+ try {
+    if(!fs.existsSync(path.join(__dirname, "..", "log"))){
+        await fspromises.mkdir(path.join(__dirname, "..", "log"))
+     }
+     await fspromises.appendFile(path.join(__dirname, "..", "log", logFile), logItem)
+ } catch (error) {
+    console.error(error)
+ }
+ 
+}
+
+const logger = (req,res,next)=>{
+    logAction(`${reg.method}\t${req.headers.origin}\t${req.url}`, "eventsLog.txt");
+    next()
+}
+
+module.exports = logger
